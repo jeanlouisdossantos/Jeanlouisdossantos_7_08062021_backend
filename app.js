ddbcebdae40fb7cede5b2f3f28aac7296fd58d99
JEAN LOUIS DOS SANTOS <jeanlouis.dossantos84@gmail.com>
2021-06-13 16:10:09 +0200

    mise en place BDD locale

--- app.js
+++ app.js
@@ -1,5 +1,27 @@
 const express = require("express");
 const bodyparser = require("body-parser");
+const Sequelize = require("sequelize");
+const env = require("dotenv");
+env.config()
+
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASS, {
+    host: process.env.DB_HOST,
+    dialect: 'mysql'
+  });
+
+async function initdatabaseconnection (){
+    console.log(process.env)
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+      } catch (error) {
+        console.error('Unable to connect to the database:', error);
+      }
+    
+}
+
+initdatabaseconnection()
 
 const app = express();
 
